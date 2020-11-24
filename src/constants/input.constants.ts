8f69ce38b00c0c955b9df87d67bae227dc2c135d
Surya Elavazhagan <kingslayer@Suryas-MacBook-Air.local>
2020-11-25 03:27:16 +0530

    feat: added support for text input editor

---
+++ src/constants/input.constants.ts
@@ -0,0 +1,24 @@
+import { SelectOptions } from "../interfaces/Select";
+
+export const INPUT_TYPES: SelectOptions[] = [
+  {
+    label: 'Text',
+    value: 'text'
+  },
+  {
+    label: 'Password',
+    value: 'password'
+  },
+  {
+    label: 'URL',
+    value: 'url'
+  },
+  {
+    label: 'Telephone',
+    value: 'tel'
+  },
+  {
+    label: 'Number',
+    value: 'number'
+  }
+];
