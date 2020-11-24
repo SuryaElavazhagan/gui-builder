f0dabd8a4d1e3b9460cac57df4857d0e2fcf0b66
Surya Elavazhagan <kingslayer@Suryas-MacBook-Air.local>
2020-11-24 22:11:19 +0530

    fix: observing selected elements for resize and re-painted click mask

--- src/services/Mask.service.ts
+++ src/services/Mask.service.ts
@@ -1,15 +1,20 @@
 import { DOM } from "../constants/dom.constants";
 import { drawOver } from "../utilities/ui";
+import { ResizeObserver } from 'resize-observer';
+import { ResizeObserverEntry } from "resize-observer/lib/ResizeObserverEntry";
 
 class MaskService {
   private click: HTMLElement;
   private hover: HTMLElement;
+  private rObserver: ResizeObserver;
 
   constructor() {
     this.click = document.createElement('div');
     this.click.id = DOM.MASK.CLICK;
     this.hover = document.createElement('div');
     this.hover.id = DOM.MASK.HOVER;
+    this.handleResize = this.handleResize.bind(this);
+    this.rObserver = new ResizeObserver(this.handleResize);
 
     this.hideClickMask();
     this.hideHoverMask();
@@ -17,21 +22,30 @@ class MaskService {
     document.body.append(this.click, this.hover);
   }
 
-  paintClickMask(ref: HTMLElement) {
+  private handleResize(entries: ResizeObserverEntry[]) {
+    if (entries.length > 0) {
+      const target = entries[0].target as HTMLElement;
+      this.paintClickMask(target);
+    }
+  }
+
+  public paintClickMask(ref: HTMLElement) {
     this.click.style.display = 'block';
+    this.rObserver.observe(ref);
     drawOver(this.click, ref);
   }
 
-  hideClickMask() {
+  public hideClickMask() {
+    this.rObserver.disconnect();
     this.click.style.display = 'none';
   }
 
-  paintHoverMask(ref: HTMLElement) {
+  public paintHoverMask(ref: HTMLElement) {
     this.hover.style.display = 'block';
     drawOver(this.hover, ref);
   }
 
-  hideHoverMask() {
+  public hideHoverMask() {
     this.hover.style.display = 'none';
   }
 }
